0cf954834711a1365887a23098e9e2a4fc490b3b
alexismenanieves <alexismenanieves@hotmail.com>
2022-05-03 09:52:21 -0500

    Added shinyApp function

--- Hello_Reactive.R
+++ Hello_Reactive.R
@@ -1,11 +1,20 @@
 library(shiny)
+library(dslabs)
 
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices=ls("package:dslabs")),
+  selectInput("dataset", label = "Dataset", choices = ls("package:dslabs")),
   verbatimTextOutput("summary"),
   tableOutput("table")
 )
 
-server <- function(input, output, session){
+server <- function(input, output, server)({
+  dataset <- reactive(
+    get(input$dataset, "package:dslabs")
+  )
   
-}
+  output$summary <- renderPrint(summary(dataset()))
+  
+  output$table <- renderTable(dataset())
+})
+
+shinyApp(ui, server)
