4e60b9a1fba066d2929d43959f86ad7d9237859b
alexismenanieves <alexismenanieves@hotmail.com>
2022-05-03 09:59:05 -0500

    Added the interface for exercise

---
+++ Shiny_Greetings.R
@@ -0,0 +1,29 @@
+# Create an app that greets the user by name. You don’t know all the 
+# functions you need to do this yet, so I’ve included some lines of code 
+# below. Think about which lines you’ll use and then copy and paste them 
+# into the right place in a Shiny app.
+
+library(shiny)
+
+ui <- fluidPage(
+  numericInput("age", "How old are you?", value = NA),
+  textInput("name", "What's your name?"),
+  textOutput("greeting"),
+  plotOutput("histogram")
+)
+
+server <- function(input, output, server)({
+  output$greeting <- renderText({
+    paste0("Hello ", input$name)
+  })
+  
+  output$histogram <- renderPlot({
+    hist(rnorm(1000))
+  }, res = 96)
+  
+})
+
+shinyApp(ui, server)
+
+
+
