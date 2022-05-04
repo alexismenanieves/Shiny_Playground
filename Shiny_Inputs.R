b9c73e637551d16669a8f9ec82ae90770d36a6a2
alexismenanieves <alexismenanieves@hotmail.com>
2022-05-04 09:57:43 -0500

    Check difference between text (cat) and verbatim (print)

--- Shiny_Inputs.R
+++ Shiny_Inputs.R
@@ -2,15 +2,19 @@ library(shiny)
 library(dslabs)
 
 ui <- fluidPage(
+  # There's an input
   selectInput("numbers","Select a number", choices = c(1,2,3,4,5)),
   shiny::textInput("text", "Type a text"),
+  # There must be an output
   textOutput("numbers"),
-  textOutput("text")
+  textOutput("text"),
+  verbatimTextOutput("summary")
 )
 
 server <- function(input, output, session){
   output$numbers <- renderText(input$numbers)
   output$text <- renderText(toupper(input$text))
+  output$summary <- renderPrint(summary(1:10))
 }
 
 shinyApp(ui, server)
